54ae2902140bd68eca2f4ef300ee78d6439177c8
uharston <uharston@gmail.com>
2020-06-27 18:28:15 -0500

    A user can now update their password - neatened up the view's code.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,30 @@ class UsersController < ApplicationController
     end 
   end 
 
+  get '/users/:id/password' do 
+    
+    if logged_in? 
+      @user = User.find_by_id(params[:id])
+      if @user == current_user
+        erb :'/users/password.html'
+      else 
+        redirect '/skate_spots'
+      end 
+    else 
+      redirect '/login'
+    end 
+
+  end 
+
+  post '/users/:id' do 
+    if current_user.authenticate(params[:old_password])
+      current_user.update(password: params[:password], password_confirmation: params[:password_confirmation])
+      redirect "/users/#{current_user.id}"
+    else
+      redirect "/users/#{current_user.id}/password"
+    end
+  end 
+
   
 
 end
