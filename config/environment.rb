3ffed4255faa813d35250c1342ac34f75418717a
uharston <uharston@gmail.com>
2020-10-28 15:40:09 -0500

    heroku config

--- config/environment.rb
+++ config/environment.rb
@@ -1,24 +1,23 @@
-require 'bundler/setup'
-Bundler.require
-configure :development do
- ENV['SINATRA_ENV'] ||= "development"
-require 'bundler/setup'
- Bundler.require(:default, ENV['SINATRA_ENV'])
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
- )
-end
+ENV['SINATRA_ENV'] ||= "development"
 
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+Dotenv.load if ENV['SINATRA_ENV'] == 'development'
 
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
+# set :environment, ENV['SINATRA_ENV'].to_sym
+
+# database_config = {
+#   :development => {
+#     :adapter => 'sqlite3',
+#     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+#   }
+# }
+
+set :database_file, "./database.yml"
+
+# set :database, database_config
+
+# require 'rack-flash'
+
+require_relative '../app/controllers/application_controller.rb'
+require_all 'app'
